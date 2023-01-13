6a5881babafa8c16e3e466feade7323946a0e189
nattvii <natt.mmd@gmail.com>
2023-01-13 19:30:03 +0300

    ex3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Fill in a number 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+    while (i <= n)
+        {
+            Console.Write(i * i * i + " ");
+            i++;
+        }
+
